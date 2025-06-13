74f872fb7b55736ce4afcffc03d3c138a0664d6c
Myxa83 <galina.nosakova.83@gmail.com>
2025-06-13 22:58:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ def start_bots():
     subprocess.Popen(["python", "bot_raid.py"])
     subprocess.Popen(["python", "bot_timezone.py"])
     # subprocess.Popen(["python", "bot_vell.py"])
-    # subprocess.Popen(["python", "bot_welcome.py"])
+    subprocess.Popen(["python", "bot_welcome.py"])
 
 if __name__ == "__main__":
     start_bots()
