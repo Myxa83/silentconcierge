f7546d14fa4cc5dd04a76c04c348091f20ba34cd
Myxa83 <galina.nosakova.83@gmail.com>
2025-06-14 10:45:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,37 @@
-import subprocess
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
-# Запуск окремих ботів
-def start_bots():
-    # Запуск бота для рейдів
-    subprocess.Popen(["python", "bot_raid.py"])
-    subprocess.Popen(["python", "bot_timezone.py"])
-    # subprocess.Popen(["python", "bot_vell.py"])
-    subprocess.Popen(["python", "bot_welcome.py"])
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.guilds = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Бот увійшов як {bot.user}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"🔄 Slash-команди синхронізовано: {len(synced)}")
+    except Exception as e:
+        print(f"❌ Помилка синхронізації команд: {e}")
+
+async def load_all_cogs():
+    await bot.load_extension("vell_cog")
+    await bot.load_extension("raid_cog")
+    await bot.load_extension("post_cog")
+    await bot.load_extension("timezone_cog")
+    await bot.load_extension("welcome_cog")
 
 if __name__ == "__main__":
-    start_bots()
+    import asyncio
+    async def main():
+        await load_all_cogs()
+        await bot.start(TOKEN)
+    asyncio.run(main())
